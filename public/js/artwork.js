223805d1c14cb1a5d14fd358f1d49b1e1f2b7074
arnaud <arnaud.valentino.humengmail.com>
2024-09-28 18:03:51 +0200

    avancement presque temriné

--- public/js/artwork.js
+++ public/js/artwork.js
@@ -1,14 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const categoryButtons = document.querySelectorAll('.category-button');
     const subCategoryButtons = document.querySelectorAll('.subcategory-button');
     const artworkContainer = document.getElementById('artwork-container');
     const imageModal = document.getElementById('imageModal');
     const modalImage = document.getElementById('modalImage');
     const closeModalButtons = document.querySelectorAll('.close-modal');
     const spinnerContainer = document.getElementById('spinner');
+    const hamburgerButton = document.getElementById('hamburger');
+    const sidebar = document.getElementById('sidebar-multi-level-sidebar');
+      // Ouvre/ferme le menu déroulant sur mobile
+ 
 
     // Fonction pour afficher la modal avec l'image en grand
-    function openModal(imageSrc) {
+    function openModal(imageSrc, title, description) {
         modalImage.src = imageSrc;
+        document.getElementById('modalTitle').textContent = title;
+        document.getElementById('modalDescription').textContent = description;
+    
         imageModal.classList.remove('hidden');
         setTimeout(() => {
             imageModal.style.opacity = '1';
@@ -29,23 +37,130 @@ document.addEventListener('DOMContentLoaded', function () {
         button.addEventListener('click', closeModal);
     });
 
-    subCategoryButtons.forEach(button => {
+    // Fonction pour attacher un événement 'click' à chaque image
+    function attachImageClickEvent(imgElement) {
+        imgElement.addEventListener('click', function () {
+            console.log("Image clicked:", this.src);
+            openModal(this.src);
+        });
+    }
+
+    // Gestionnaire de clic pour les catégories (inclut la catégorie "All")
+    categoryButtons.forEach(button => {
         button.addEventListener('click', function (event) {
             event.preventDefault();
-            const subCategoryId = this.getAttribute('data-sub-category-id');
-            console.log(`Sub-category clicked: ${subCategoryId}`);
+            const categoryId = this.getAttribute('data-category-id');
 
             // Effacer le contenu existant
             artworkContainer.innerHTML = ''; 
             
-            // Forcer l'affichage du spinner directement via le style
-            spinnerContainer.style.display = 'flex';  // Forcer l'affichage en flex pour centrer le spinner
-            spinnerContainer.style.zIndex = '1000';   // S'assurer qu'il est au-dessus de tout
+            // Afficher le spinner
+            spinnerContainer.style.display = 'flex';
+            spinnerContainer.style.zIndex = '1000';
 
             // Masquer le conteneur tant que les images ne sont pas chargées
             artworkContainer.style.opacity = '0';
             artworkContainer.style.transition = 'opacity 1s ease';
 
+            // Requête pour récupérer les œuvres d'art d'une catégorie (ou "All")
+            fetch(`/category/${categoryId}/artworks`)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    if (data.length === 0) {
+                        spinnerContainer.style.display = 'none';
+                        artworkContainer.innerHTML = '<p class="text-center text-2xl text-gray-500">Aucun artwork disponible.</p>';
+                    } else {
+                        let imagesLoaded = 0;
+                        const totalImages = data.length;
+
+                        data.forEach((artwork, index) => {
+                            const artworkElement = document.createElement('div');
+                            artworkElement.classList.add('artwork-item', 'relative', 'bg-gray-100', 'rounded', 'shadow-lg', 'overflow-hidden');
+                            artworkElement.style.opacity = '0';
+                            artworkElement.style.transform = 'translateY(20px)';
+                            artworkElement.style.transition = 'opacity 1s ease, transform 1s ease';
+
+                            artworkElement.innerHTML = `
+                                <img src="/uploads/images/${artwork.imagePath}" alt="${artwork.title}" class="w-full h-full object-cover cursor-pointer hidden">
+                                <div class="absolute bottom-0 left-0 w-full p-4 bg-gradient-to-t from-black to-transparent text-white"></div>
+                            `;
+
+                            const imgElement = artworkElement.querySelector('img');
+                            artworkContainer.appendChild(artworkElement);
+
+                            if (imgElement.complete) {
+                                imagesLoaded += 1;
+                                imgElement.classList.remove('hidden');
+                                setTimeout(() => {
+                                    artworkElement.style.opacity = '1';
+                                    artworkElement.style.transform = 'translateY(0)';
+                                }, index * 200);
+
+                                attachImageClickEvent(imgElement);
+
+                                if (imagesLoaded === totalImages) {
+                                    spinnerContainer.style.display = 'none';
+                                    artworkContainer.style.opacity = '1';
+                                }
+                            } else {
+                                imgElement.onload = function () {
+                                    imagesLoaded += 1;
+                                    imgElement.classList.remove('hidden');
+                                    setTimeout(() => {
+                                        artworkElement.style.opacity = '1';
+                                        artworkElement.style.transform = 'translateY(0)';
+                                    }, index * 200);
+
+                                    attachImageClickEvent(imgElement);
+
+                                    if (imagesLoaded === totalImages) {
+                                        spinnerContainer.style.display = 'none';
+                                        artworkContainer.style.opacity = '1';
+                                    }
+                                };
+
+                                imgElement.onerror = function () {
+                                    imagesLoaded += 1;
+                                    if (imagesLoaded === totalImages) {
+                                        spinnerContainer.style.display = 'none';
+                                        artworkContainer.style.opacity = '1';
+                                    }
+                                };
+                            }
+                        });
+                    }
+                })
+                .catch(error => {
+                    console.error('Erreur lors de la récupération des artworks:', error);
+                    spinnerContainer.style.display = 'none';
+                    artworkContainer.innerHTML = '<p class="text-center text-2xl text-red-500">Erreur lors de la récupération des artworks.</p>';
+                });
+        });
+    });
+
+    // Gestionnaire de clic pour les sous-catégories
+    subCategoryButtons.forEach(button => {
+        button.addEventListener('click', function (event) {
+            event.preventDefault();
+            const subCategoryId = this.getAttribute('data-sub-category-id');
+
+            // Effacer le contenu existant
+            artworkContainer.innerHTML = ''; 
+            
+            // Afficher le spinner
+            spinnerContainer.style.display = 'flex';
+            spinnerContainer.style.zIndex = '1000';
+
+            // Masquer le conteneur tant que les images ne sont pas chargées
+            artworkContainer.style.opacity = '0';
+            artworkContainer.style.transition = 'opacity 1s ease';
+
+            // Requête pour récupérer les œuvres d'art d'une sous-catégorie spécifique
             fetch(`/subcategory/${subCategoryId}/artworks`)
                 .then(response => {
                     if (!response.ok) {
@@ -55,7 +170,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 })
                 .then(data => {
                     if (data.length === 0) {
-                        spinnerContainer.style.display = 'none'; // Cacher le spinner
+                        spinnerContainer.style.display = 'none';
                         artworkContainer.innerHTML = '<p class="text-center text-2xl text-gray-500">Aucun artwork disponible pour cette sous-catégorie.</p>';
                     } else {
                         let imagesLoaded = 0;
@@ -64,81 +179,79 @@ document.addEventListener('DOMContentLoaded', function () {
                         data.forEach((artwork, index) => {
                             const artworkElement = document.createElement('div');
                             artworkElement.classList.add('artwork-item', 'relative', 'bg-gray-100', 'rounded', 'shadow-lg', 'overflow-hidden');
-                            artworkElement.style.opacity = '0'; // Commence invisible
-                            artworkElement.style.transform = 'translateY(20px)'; // Commence décalé vers le bas
-                            artworkElement.style.transition = 'opacity 1s ease, transform 1s ease'; // Transition animée sur 1 seconde
+                            artworkElement.style.opacity = '0';
+                            artworkElement.style.transform = 'translateY(20px)';
+                            artworkElement.style.transition = 'opacity 1s ease, transform 1s ease';
 
-                            // Masquer directement l'image jusqu'à son chargement
                             artworkElement.innerHTML = `
-                                <img src="/uploads/images/${artwork.imagePath}" alt="${artwork.title}" class="w-full h-full object-cover cursor-pointer hidden"> <!-- Cacher l'image -->
-                                <div class="absolute bottom-0 left-0 w-full p-4 bg-gradient-to-t from-black to-transparent text-white">
-                                </div>
+                                <img src="/uploads/images/${artwork.imagePath}" alt="${artwork.title}" class="w-full h-full object-cover cursor-pointer hidden">
+                                <div class="absolute bottom-0 left-0 w-full p-4 bg-gradient-to-t from-black to-transparent text-white"></div>
                             `;
 
                             const imgElement = artworkElement.querySelector('img');
-
-                            // Ajoute l'élément au conteneur immédiatement
                             artworkContainer.appendChild(artworkElement);
 
-                            // Vérifier si l'image est déjà en cache
                             if (imgElement.complete) {
-                                // Si l'image est dans le cache, déclencher manuellement l'animation
                                 imagesLoaded += 1;
-                                imgElement.classList.remove('hidden'); // Montrer l'image
+                                imgElement.classList.remove('hidden');
                                 setTimeout(() => {
-                                    artworkElement.style.opacity = '1'; // Rendre l'image visible
-                                    artworkElement.style.transform = 'translateY(0)'; // Faire remonter l'image
-                                }, index * 200); // Décalage de 0,2 seconde entre chaque image
+                                    artworkElement.style.opacity = '1';
+                                    artworkElement.style.transform = 'translateY(0)';
+                                }, index * 200);
 
-                                // Si toutes les images sont chargées, cacher le spinner et afficher le conteneur
+                                attachImageClickEvent(imgElement);
+
                                 if (imagesLoaded === totalImages) {
                                     spinnerContainer.style.display = 'none';
-                                    artworkContainer.style.opacity = '1'; // Rendre visible le conteneur
+                                    artworkContainer.style.opacity = '1';
                                 }
                             } else {
-                                // Attends que chaque image soit chargée avant de la montrer
                                 imgElement.onload = function () {
                                     imagesLoaded += 1;
-
-                                    imgElement.classList.remove('hidden'); // Montrer l'image
-
-                                    // Applique l'animation après un délai décalé pour chaque image
+                                    imgElement.classList.remove('hidden');
                                     setTimeout(() => {
-                                        artworkElement.style.opacity = '1'; // Rendre l'image visible
-                                        artworkElement.style.transform = 'translateY(0)'; // Faire remonter l'image
-                                    }, index * 200); // Décalage de 0,2 seconde entre chaque image
+                                        artworkElement.style.opacity = '1';
+                                        artworkElement.style.transform = 'translateY(0)';
+                                    }, index * 200);
 
-                                    // Si toutes les images sont chargées, cacher le spinner et afficher le conteneur
+                                    attachImageClickEvent(imgElement);
+
                                     if (imagesLoaded === totalImages) {
                                         spinnerContainer.style.display = 'none';
-                                        artworkContainer.style.opacity = '1'; // Rendre visible le conteneur
+                                        artworkContainer.style.opacity = '1';
                                     }
                                 };
 
-                                // Gère les erreurs de chargement d'image
                                 imgElement.onerror = function () {
                                     imagesLoaded += 1;
-
-                                    // Si toutes les images ont tenté de se charger (y compris les erreurs), cacher le spinner
                                     if (imagesLoaded === totalImages) {
                                         spinnerContainer.style.display = 'none';
-                                        artworkContainer.style.opacity = '1'; // Rendre visible le conteneur même en cas d'erreurs
+                                        artworkContainer.style.opacity = '1';
                                     }
                                 };
                             }
-
-                            // Ajoute un gestionnaire d'événements pour ouvrir la modal quand l'image est cliquée
-                            imgElement.addEventListener('click', function () {
-                                openModal(this.src);
-                            });
                         });
                     }
                 })
                 .catch(error => {
                     console.error('Erreur lors de la récupération des artworks:', error);
+                    spinnerContainer.style.display = 'none';
                     artworkContainer.innerHTML = '<p class="text-center text-2xl text-red-500">Erreur lors de la récupération des artworks.</p>';
-                    spinnerContainer.style.display = 'none'; // Cacher le spinner
                 });
         });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const sidebar = document.getElementById('sidebar');
+    const toggleSidebarButton = document.getElementById('toggleSidebar');
+    
+    toggleSidebarButton.addEventListener('click', function () {
+        // Si la sidebar est cachée, la faire apparaître
+        if (sidebar.classList.contains('-translate-x-full')) {
+            sidebar.classList.remove('-translate-x-full');
+        } else {
+            sidebar.classList.add('-translate-x-full');
+        }
     });
 });
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,8 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\SubCategory;
+use App\Repository\ArtworkRepository;
 use App\Repository\CategoryRepository;
-use App\Repository\SubCategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,45 +14,50 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(CategoryRepository $categoryRepository): Response
+    public function index(CategoryRepository $categoryRepository, ArtworkRepository $artworkRepository): Response
     {
         $categories = $categoryRepository->findAll();
+        $artworks = $artworkRepository->findAll();
+
+        // Rechercher la catégorie "All" et la mettre en première position
+    usort($categories, function ($a, $b) {
+        return $a->getName() === 'All' ? -1 : 1; // Si c'est "All", on le met en premier
+    });
 
         return $this->render('home/index.html.twig', [
             'categories' => $categories,
+            'artworks' => $artworks,
         ]);
     }
 
-    #[Route('/category/{id}/subcategories', name: 'get_subcategories', methods: ['GET'])]
-    public function getSubCategories(Category $category): JsonResponse
-    {
-        $subCategories = $category->getSubCategories();
-        $subCategoriesArray = [];
-
-        foreach ($subCategories as $subCategory) {
-            $subCategoriesArray[] = [
-                'id' => $subCategory->getId(),
-                'name' => $subCategory->getName(),
-            ];
-        }
-
-        return new JsonResponse($subCategoriesArray);
-    }
-
     #[Route('/category/{id}/artworks', name: 'get_category_artworks', methods: ['GET'])]
-    public function getCategoryArtworks(Category $category): JsonResponse
+    public function getCategoryArtworks($id, CategoryRepository $categoryRepository, ArtworkRepository $artworkRepository): JsonResponse
     {
-        $artworks = $category->getArtworks();
+        // Récupère la catégorie avec l'ID
+        $category = $categoryRepository->find($id);
+    
+        // Si la catégorie est "All", récupérer toutes les œuvres
+        if ($category && $category->getName() === 'All') {
+            $artworks = $artworkRepository->findAll(); // Récupère toutes les œuvres
+        } else {
+            // Sinon, récupérer les œuvres d'une catégorie spécifique
+            if (!$category) {
+                return new JsonResponse(['error' => 'Category not found'], 404);
+            }
+            $artworks = $category->getArtworks();
+        }
+    
+        // Transformer les œuvres d'art en tableau JSON
         $artworksArray = [];
-
         foreach ($artworks as $artwork) {
             $artworksArray[] = [
                 'id' => $artwork->getId(),
                 'title' => $artwork->getTitle(),
                 'imagePath' => $artwork->getImagePath(),
+                'description' => $artwork->getDescription(),
             ];
         }
-
+    
         return new JsonResponse($artworksArray);
     }
 
